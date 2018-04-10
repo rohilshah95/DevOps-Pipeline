da7f871d25c55693156a6eabc9ece3ec61ab74bf
Rohil Shah <rshah8@ncsu.edu>
2018-04-09 23:13:42 -0400

    adding alert to load balancer

--- loadBalancer.js
+++ loadBalancer.js
@@ -2,15 +2,24 @@ var httpProxy = require("http-proxy");
 var http = require('http');
 
 var proxy = httpProxy.createProxyServer({});
-
+var alert=false;
 var loadCounter=1;
 var server=http.createServer( function (req, res){
-	
-	if(loadCounter%3==0)
+	if(!alert)
 	{
-		proxy.web(req, res, {target: 'http://{{canary_ip}}'}, function (e){
-			proxy.web(req, res, {target: 'http://{checkbox_ip}'});
-		});
+		if(loadCounter%3==0)
+		{
+			proxy.web(req, res, {target: 'http://{{canary_ip}}'}, function (e){
+				proxy.web(req, res, {target: 'http://{checkbox_ip}'});
+			});
+		}
+		else
+		{
+			proxy.web(req, res, {target: 'http://{{checkbox_ip}}'}, function (e){
+				console.log(e);
+			});
+		}
+		loadCounter++;
 	}
 	else
 	{
@@ -18,8 +27,19 @@ var server=http.createServer( function (req, res){
 			console.log(e);
 		});
 	}
-	loadCounter++;
-
 });
+
+var checkStatus = setInterval(function () {
+	try {
+		http.get('http://{{canary_ip}}', function (res) {
+			//Working fine
+		}).on('error', function (e) {
+			alert = true;
+			clearInterval(checkStatus);
+		});
+	} catch (e) {
+		alert = true;
+	}
+}, 5000);
 
 server.listen(80);
