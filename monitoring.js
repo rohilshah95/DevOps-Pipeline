41aa0353e409ea98653607ef87e90fed2fc08825
sshah14 <sshah14@ncsu.edu>
2018-04-14 17:06:27 -0400

    Cleanup

--- monitoring.js
+++ monitoring.js
@@ -23,50 +23,6 @@ exec("node fastService.js");
 exec("node mediumService.js");
 exec("node slowService.js");
 
-function memoryLoad()
-{
-	// console.log( os.totalmem(), os.freemem() );
-	return 0;
-}
-
-// Create function to get CPU information
-function cpuTicksAcrossCores() 
-{
-  //Initialise sum of idle and time of cores and fetch CPU info
-  var totalIdle = 0, totalTick = 0;
-  var cpus = os.cpus();
- 
-  //Loop through CPU cores
-  for(var i = 0, len = cpus.length; i < len; i++) 
-  {
-		//Select CPU core
-		var cpu = cpus[i];
-		//Total up the time in the cores tick
-		for(type in cpu.times) 
-		{
-			totalTick += cpu.times[type];
-		}     
-		//Total up the idle time of the core
-		totalIdle += cpu.times.idle;
-  }
- 
-  //Return the average Idle and Tick times
-  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length};
-}
-
-var startMeasure = cpuTicksAcrossCores();
-
-function cpuAverage()
-{
-	var endMeasure = cpuTicksAcrossCores(); 
- 
-	//Calculate the difference in idle and total time between the measures
-	var idleDifference = endMeasure.idle - startMeasure.idle;
-	var totalDifference = endMeasure.total - startMeasure.total;
- 
-	//Calculate the average percentage CPU usage
-	return 0;
-}
 
 function measureLatenancy(server)
 {
@@ -124,7 +80,7 @@ io.on('connection', function (socket) {
 	var heartbeatTimer = setInterval( function () 
 	{
 		var data = { 
-			name: "iTrust2 Monitoring", cpu: cpuAverage(), memoryLoad: memoryLoad()
+			name: "iTrust2 Monitoring"
 			,nodes: calculateColor()
 		};
 		console.log("interval", data)
